a3f87ada59c48a8359f7fdde3fb877aa8f754433
ADR0702 <catalinadriandumitru@outlook.com>
2026-01-29 15:56:52 +0200

    new exercises

--- Link Recap/Recap-Miniapps/Volume 9/4.interview_ex.py
+++ Link Recap/Recap-Miniapps/Volume 9/4.interview_ex.py
@@ -1,12 +1,12 @@
-# numbers = [3, 5, 3, 7, 5, 9, 3]
-# new_list=[]
+numbers = [3, 5, 3, 7, 5, 9, 3]
+new_list=[]
 
-# for no in range(len(numbers)):
-#     if numbers[no] not in new_list:
-#         new_list.append(numbers[no])
+for no in range(len(numbers)):
+    if numbers[no] not in new_list:
+        new_list.append(numbers[no])
         
 
-# print(new_list)
+print(new_list)
 
 
 text = "Ana are 2 mere si 3 pere"
